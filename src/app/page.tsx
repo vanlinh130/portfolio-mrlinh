c6ab166a388ff98ee696543cdf6ccb175d101ca8
vanlinh130 <linhvanle130@gmail.com>
2025-03-31 02:02:09 +0700

    layout components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from "next/image";
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Content from "@/components/content";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Portfolio from "@/components/portfolio";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
-    <div className="text-amber-300">Văn Linh</div>
+    <div className="">
+      <div className="">
+        <Header/>
+        <div className="section-center">
+          <Content/>
+          <About/>
+          <Skills/>
+          <Portfolio/>
+          <Contact/>
+        </div>
+        <Footer/>
+      </div>
+    </div>
   );
 }
---
+++ src/components/content.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div className='pt-20'>Content</div>
+  )
+}
+
+export default Content
